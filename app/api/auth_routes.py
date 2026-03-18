0d6580a7795b65fea20a4a22de6c5b46af30b986
Seth Dayao <dayao.seth@gmail.com>
2026-03-18 08:32:40 +0800

    fix(api): updated for Oauth2

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+from app.dependencies.auth_dependencies import get_current_user
+from app.db import models
 
 from app.db.database import SessionLocal
 from app.schemas.auth_schema import UserCreate, UserLogin
@@ -28,15 +31,27 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
 
 
 @router.post("/login")
-def login(user: UserLogin, db: Session = Depends(get_db)):
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
 
     token = authenticate_user(
         db,
-        user.username,
-        user.password
+        form_data.username,
+        form_data.password
     )
 
     return {
         "access_token": token,
         "token_type": "bearer"
+    }
+
+@router.get("/profile")
+def get_profile(current_user: models.User = Depends(get_current_user)):
+
+    return {
+        "id": current_user.id,
+        "username": current_user.username,
+        "email": current_user.email,
     }
