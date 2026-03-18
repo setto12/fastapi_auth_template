9788efc8eb9e457c89de37e358e186eb77854bdf
Seth Dayao <dayao.seth@gmail.com>
2026-03-18 08:48:44 +0800

    chore: update CORS for local react frontend

--- app/main.py
+++ app/main.py
@@ -9,8 +9,8 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000",   # React default
-    "http://127.0.0.1:3000",
+    "http://localhost:5173",  
+    "http://127.0.0.1:5173",
 ]
 
 app.add_middleware(
